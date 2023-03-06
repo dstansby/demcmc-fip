a5a4b2cddf38bc6d17eaf564cf3561d036aabfba
David Stansby <dstansby@gmail.com>
2023-03-06 16:08:47 +0000

    Add a Myriad submission script

---
+++ submit_demcmc_myriad.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+# Change this to point to the location of the DEM script
+DEM_SCRIPT = "/home/ucasdst/fip/run_dem.py"
+# Change this to set the number of cores requested. Max
+# on Myriad is 36, but much easier to set to a lower
+# number (e.g. 18) to avoid a long wait in a the queue
+N_CORES = "16"
+
+# Request wallclock time (format hours:minutes:seconds).
+#$ -l h_rt=12:00:0
+
+# Request RAM (must be an integer followed by M, G, or T)
+#$ -l mem=2G
+
+# Request TMPDIR space (default is 10 GB - remove if cluster is diskless)
+#$ -l tmpfs=10G
+
+# Request cores
+#$ -pe smp $N_CORES
+
+# Set the name of the job.
+#$ -N demcmc
+
+# Set the working directory to somewhere in your scratch space.
+# This is a necessary step as compute nodes cannot write to $HOME.
+#$ -wd /home/$(whoami)/Scratch
+
+
+# Your work should be done in $TMPDIR
+cd $TMPDIR
+
+module load "python3/3.9"
+virtualenv ./fipenv
+source ./fipenv/bin/activate
+
+which python
+python -m pip install --upgrade pip
+python -m pip install /home/ucasdst/fip/demcmc
+python -m pip install scipy multiprocessing-logging
+
+date
+python $DEM_SCRIPT
