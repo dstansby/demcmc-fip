8e7c65886ae105945b69784ec5f266f6e4c3ab70
David Stansby <dstansby@gmail.com>
2023-03-07 16:19:45 +0000

    Add logging to fip calc

--- run_fip.py
+++ run_fip.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import numpy as np
 import tqdm
+import logging
 
 import astropy.units as u
 from scipy.io import readsav
@@ -20,10 +21,23 @@ from fiplib import parse_line
 input_data_path = Path(__file__).parent / "data_in"
 # The DEM data path should contain the .nc files with the DEMs
 dem_path = Path(__file__).parent / "dems"
+# Path where output array and log will be saved
+output_data_path = Path(__file__).parent / "fip_out"
 
+output_data_path.mkdir(exist_ok=True)
 fip_lines = np.load(input_data_path / "intensities.npy", allow_pickle=True).tolist()
 cont_func_data = readsav(str(input_data_path / "emissivity.sav"))
 cont_func_temps = np.logspace(4, 8, 401) * u.K
+
+
+# Setup logging
+logging.basicConfig(
+    filename=str(output_data_path / "run_fip.log"),
+    encoding="utf-8",
+    level=logging.INFO,
+    format="%(asctime)s %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+)
 
 
 def get_cont_funcs(xpix: int, ypix: int):
@@ -76,13 +90,12 @@ xs, ys = xys[0].ravel(), xys[1].ravel()
 for x, y in tqdm.tqdm(zip(xs, ys), total=len(xs)):
     fname = dem_path / f"dem_{x}_{y}.nc"
     if not fname.exists():
-        continue
-    if np.isfinite(fip_array[x, y]):
+        logging.info(f"No DEM file exists for pixel ({x}, {y})")
         continue
     dem = DEMOutput.load(fname)
     lines = get_lines(x, y)
     if not np.all(np.array([line.intensity_obs for line in lines.values()]) > 0):
-        # Either Si or S line has zero intensity
+        logging.info(f"Either S or Si intensity is zero for pixel ({x}, {y})")
         continue
 
     # Iterate through all the DEM samples, and calculate the FIP bias
@@ -97,12 +110,15 @@ for x, y in tqdm.tqdm(zip(xs, ys), total=len(xs)):
         fips.append(fip * correction)
 
     if not len(fips):
+        logging.info(f"Predicted Si intensities are all zero for pixel ({x}, {y})")
         continue
 
     # Take the mean of all the sampled FIP biases. Could take another
     # measure here (e.g. the median), or save the whole sample set,
     # or calculate the standard deviation of samples (or something
     # else I haven't thought of!)
+    logging.info(f"Saving mean FIP bias for pixel ({x}, {y})")
     fip_array[x, y] = np.mean(fips)
 
-np.save("fip_array.npy", fip_array)
+# Save array
+np.save(str(output_data_path / "fip_array.npy"), fip_array)
